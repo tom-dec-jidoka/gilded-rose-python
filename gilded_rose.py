050b8d0cc33e0e3fb1436433ff6733ccee1a4134
Tom Decoutere <tom.decoutere@jidoka.be>
2022-12-07 20:47:38 +0100

    sulfuras item

--- gilded_rose.py
+++ gilded_rose.py
@@ -13,9 +13,11 @@ class GildedRose(object):
 class ItemFactory:
     def create(name, sell_in, quality):
         if(name == AGED_BRIE):
-            return AgdBrie(name, sell_in, quality)
+            return AgedBrie(name, sell_in, quality)
         elif(name == BACKSTAGE_PASS):
             return BackstagePass(name, sell_in, quality)
+        elif(name == SULFURAS):
+            return Sulfuras(name, sell_in, quality)
         else:
             return Item(name, sell_in, quality)
 class Item:
@@ -29,18 +31,15 @@ class Item:
 
     def update_quality(self):
         if self.quality > 0:
-            if self.name != SULFURAS:
-                self.quality = self.quality - 1
+            self.quality = self.quality - 1
                 
-        if self.name != SULFURAS:
-            self.sell_in = self.sell_in - 1
+        self.sell_in = self.sell_in - 1
         if self.sell_in < 0:
             if self.quality > 0:
-                if self.name != SULFURAS:
-                    self.quality = self.quality - 1
+                self.quality = self.quality - 1
             
 
-class AgdBrie(Item):
+class AgedBrie(Item):
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
@@ -84,3 +83,14 @@ class BackstagePass(Item):
         if self.sell_in < 0:
             self.quality = self.quality - self.quality    
 
+class Sulfuras(Item):
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "Hi I am Sulfuras %s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def update_quality(self):
+        pass    
