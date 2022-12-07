80151d3d8942470cdc4568c5c0e8d6332691515e
bjornmonnens <bjorn.monnens@jidoka.be>
2022-12-07 19:24:12 +0100

    adding normal and double decreasing

---
+++ test_gilded_rose_quality_decreases_twice_as_fast.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from gilded_rose import Item, GildedRose
+
+
+class GildedRoseQualityDecreasesTwiceAsFastTest(unittest.TestCase):
+    def test_normal_decrease(self):
+        items = [Item("Dummy", 2, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(1, items[0].sell_in)
+        self.assertEquals(9, items[0].quality)
+        gilded_rose.update_quality()
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(8, items[0].quality)
+        
+
+    def test_double_decrease(self):
+        items = [Item("Dummy", 2, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+        self.assertEquals(-1, items[0].sell_in)
+        self.assertEquals(6, items[0].quality)
+
+        
+if __name__ == '__main__':
+    unittest.main()
